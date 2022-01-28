6bffbee106cc986117db26c2ae8aeb5cf2e48e33
Dan Jacob <danjac2018@gmail.com>
2022-01-28 13:32:45 +0200

    Registration tests

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from django.contrib.auth import (authenticate, get_user_model,
-                                 password_validation)
+from django.contrib.auth import get_user_model, password_validation
 from django.core.exceptions import ValidationError
 
 User = get_user_model()
@@ -29,6 +28,16 @@ class UserCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["email", "name"]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        return password2
 
     def _post_clean(self):
         super()._post_clean()
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,10 @@
+import http
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, reverse_lazy
+
+from .forms import UserCreationForm
 
 User = get_user_model()
 
@@ -25,3 +30,53 @@ class TestUserModel(TestCase):
 
     def test_get_short_name(self):
         self.assertEqual(User(name="Test User").get_short_name(), "Test User")
+
+
+class TestUserCreationForm(TestCase):
+    form_data = {
+        "name": "Tester",
+        "email": "tester@gmail.com",
+        "password1": "testpass1",
+        "password2": "testpass1",
+    }
+
+    def test_save(self):
+        form = UserCreationForm(self.form_data)
+        self.assertTrue(form.is_valid())
+
+        user = form.save()
+        self.assertTrue(user.check_password("testpass1"))
+
+    def test_password_mismatch(self):
+        form = UserCreationForm(
+            {
+                **self.form_data,
+                "password2": "testpass2",
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+
+class TestRegisterView(TestCase):
+    url = reverse_lazy("register")
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
+    def test_post_invalid(self):
+        response = self.client.post(self.url, {"email": "invalid"})
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
+    def test_post_valid(self):
+        response = self.client.post(
+            self.url,
+            {
+                "name": "Tester",
+                "email": "tester@gmail.com",
+                "password1": "testpass1",
+                "password2": "testpass1",
+            },
+        )
+        self.assertRedirects(response, reverse("home"))
+        self.assertTrue(User.objects.filter(email="tester@gmail.com").exists())
